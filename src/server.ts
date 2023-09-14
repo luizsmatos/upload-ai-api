5d48d915e22e967bdd6158df38453c0e327ae695
Luiz Gustavo <gusstavopdm@gmail.com>
2023-09-14 16:28:20 -0300

    ✨ feat(server.ts): add support for new routes - upload video and create transcription

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import { fastify } from "fastify";
 import { listPromptsRoute } from "./routes/list-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const app = fastify();
 
 app.register(listPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
 
 app
   .listen({
