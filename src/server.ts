788e3a0d0ceb156835c4b365d958f46fe83e30ab
Luiz Gustavo <gusstavopdm@gmail.com>
2023-09-14 16:56:11 -0300

    ✨ feat(server.ts): add CORS support to allow requests from any origin

--- src/server.ts
+++ src/server.ts
@@ -2,14 +2,19 @@ import { fastify } from "fastify";
 import { listPromptsRoute } from "./routes/list-prompts";
 import { uploadVideoRoute } from "./routes/upload-video";
 import { createTranscriptionRoute } from "./routes/create-transcription";
-import { generateAICompletionRoute } from './routes/generate-ai-completion';
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify();
+
+app.register(fastifyCors, {
+  origin: "*",
+});
 
 app.register(listPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
-app.register(generateAICompletionRoute)
+app.register(generateAICompletionRoute);
 
 app
   .listen({
